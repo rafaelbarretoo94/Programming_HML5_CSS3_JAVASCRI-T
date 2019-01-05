91e9c0bc7a6490fd99992dda300ad38b77e884c6
Rafael Barreto <rafaelbarretoo94@gmail.com>
2019-01-05 04:19:35 -0200

    Api Conversor Alturas -JWT

---
+++ Desktop/Cursos/JWT/JWT/DAO/UserDAO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using JWT.Entities;
+
+namespace JWT.DAO
+{
+    public class UserDAO
+    {
+        private IConfiguration _configuration;
+
+        public UserDAO(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public User Find(string userID)
+        {
+            using (SqlConnection conexao = new SqlConnection(
+                _configuration.GetConnectionString("ExemploJWT")))
+            {
+                return conexao.QueryFirstOrDefault<User>(
+                    "SELECT UserID, AccessKey " +
+                    "FROM dbo.Users " +
+                    "WHERE UserID = @UserID", new { UserID = userID });
+            }
+
+        }
+    }
+}
